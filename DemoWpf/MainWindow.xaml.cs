b7a64525c866db1cd227f8d87f6f4b60e37dd5df
LorenzoCelli <lorenzo.celli00@gmail.com>
2023-03-28 11:50:26 +0200

    Fix for wpf demo

--- DemoWpf/MainWindow.xaml.cs
+++ DemoWpf/MainWindow.xaml.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using System.Windows;
+using QuickForms.Core;
 using QuickForms.Wpf;
 
 namespace DemoWpf
